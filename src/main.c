cdc7ce8b1f4fa6de39bf3063ddc9793e9cd79ffa
Talal-l <talal__l@hotmail.com>
2024-08-10 16:54:00 +0300

    init

---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include "raylib.h"
+#include "raymath.h"
+
+const int SCREEN_WIDTH = 400;
+const int SCREEN_HEIGHT = 400;
+
+int main()
+{
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "TEMPLATE");
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose())
+    {
+
+        BeginDrawing();
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
