98ff00dbe889c7683b205cbd0c6ee7f4dee421d8
HamnYce <hamadboa@gmail.com>
2024-09-27 20:00:46 +0300

    add hot reloading

--- build.zig
+++ build.zig
@@ -5,8 +5,14 @@ const raylib = @import("raylib/build.zig");
 const program_name = "TEMPLATE";
 
 pub fn build(b: *std.Build) !void {
+    // add flag to indicate we this is a hot reload
+    const hot_reload = b.option(bool, "hot_reload", "only build the game shared library") orelse false;
+
+    // Give user option to add their own flags
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
+
+    // Create main executable
     const exe = b.addExecutable(.{
         .name = program_name,
         .root_source_file = b.path("src/main.zig"),
@@ -14,8 +20,28 @@ pub fn build(b: *std.Build) !void {
         .optimize = optimize,
     });
 
-    exe.linkLibC();
+    // Create Game Dylib
+    const game_dylib = b.addSharedLibrary(.{
+        .name = "game",
+        .root_source_file = b.path("src/game.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
 
+    // link c libraries
+    exe.linkLibC();
+    game_dylib.linkLibC();
+
+    // add raylib header files
+    exe.addIncludePath(b.path("raylib/src"));
+    exe.addIncludePath(b.path("raylib/src/external"));
+    exe.addIncludePath(b.path("raylib/src/external/glfw/include"));
+
+    game_dylib.addIncludePath(b.path("raylib/src"));
+    game_dylib.addIncludePath(b.path("raylib/src/external"));
+    game_dylib.addIncludePath(b.path("raylib/src/external/glfw/include"));
+
+    // add raylib static library
     exe.addObjectFile(switch (target.result.os.tag) {
         .windows => b.path("raylib/zig-out/lib/raylib.lib"),
         .linux => b.path("raylib/zig-out/lib/libraylib.a"),
@@ -24,10 +50,15 @@ pub fn build(b: *std.Build) !void {
         else => @panic("Unsupported OS"),
     });
 
-    exe.addIncludePath(b.path("raylib/src"));
-    exe.addIncludePath(b.path("raylib/src/external"));
-    exe.addIncludePath(b.path("raylib/src/external/glfw/include"));
+    game_dylib.addObjectFile(switch (target.result.os.tag) {
+        .windows => b.path("raylib/zig-out/lib/raylib.lib"),
+        .linux => b.path("raylib/zig-out/lib/libraylib.a"),
+        .macos => b.path("raylib/zig-out/lib/libraylib.a"),
+        .emscripten => b.path("raylib/zig-out/lib/libraylib.a"),
+        else => @panic("Unsupported OS"),
+    });
 
+    // Add OS specific Libraries that raylib uses in the back
     switch (target.result.os.tag) {
         .windows => {
             std.debug.print("running on windows\n", .{});
@@ -57,32 +88,52 @@ pub fn build(b: *std.Build) !void {
             exe.linkFramework("IOKit");
 
             exe.defineCMacro("PLATFORM_DESKTOP", null);
+
+            game_dylib.linkFramework("Foundation");
+            game_dylib.linkFramework("Cocoa");
+            game_dylib.linkFramework("OpenGL");
+            game_dylib.linkFramework("CoreAudio");
+            game_dylib.linkFramework("CoreVideo");
+            game_dylib.linkFramework("IOKit");
+
+            game_dylib.defineCMacro("PLATFORM_DESKTOP", null);
         },
         else => {
             @panic("Unsupported OS");
         },
     }
 
-    var dir = try std.fs.cwd().openDir("src", .{ .iterate = true });
-    defer if (comptime builtin.zig_version.minor >= 12) dir.close();
+    // add c files in src directory
+    // var dir = try std.fs.cwd().openDir("src", .{ .iterate = true });
+    // defer if (comptime builtin.zig_version.minor >= 12) dir.close();
 
-    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
-    defer arena.deinit();
+    // var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    // defer arena.deinit();
 
-    const allocator = arena.allocator();
-    var iter = try dir.walk(allocator);
-    defer iter.deinit();
+    // const allocator = arena.allocator();
+    // var iter = try dir.walk(allocator);
+    // defer iter.deinit();
 
-    while (try iter.next()) |entry| {
-        if (entry.kind != .file) continue;
+    // while (try iter.next()) |entry| {
+    //     if (entry.kind != .file) continue;
 
-        _ = std.mem.lastIndexOf(u8, entry.basename, ".c") orelse continue;
-        const path = try std.fs.path.join(b.allocator, &.{ "src", entry.path });
+    //     _ = std.mem.lastIndexOf(u8, entry.basename, ".c") orelse continue;
+    //     const path = try std.fs.path.join(b.allocator, &.{ "src", entry.path });
 
-        std.debug.print("path {s}\n", .{path});
+    //     std.debug.print("path {s}\n", .{path});
 
-        exe.addCSourceFile(.{ .file = b.path(path), .flags = &.{} });
+    //     exe.addCSourceFile(.{ .file = b.path(path), .flags = &.{} });
+    // }
+
+    b.installArtifact(game_dylib);
+    if (!hot_reload) {
+        b.installArtifact(exe);
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+        if (b.args) |args| {
+            run_cmd.addArgs(args);
+        }
+        const run_step = b.step("run", "Run the app");
+        run_step.dependOn(&run_cmd.step);
     }
-
-    b.installArtifact(exe);
 }
--- src/main.zig
+++ src/main.zig
@@ -1,18 +1,92 @@
 const std = @import("std");
-const rl = @cImport({
+const c = @cImport({
     @cInclude("raylib.h");
 });
 
+const screen_w = 400;
+const screen_h = 200;
+
+// The main exe doesn't know anything about the GameState structure
+// because that information exists inside the DLL, but it doesn't
+// need to care. All main cares about is where it exists in memory
+// so *anyopaque is just a pointer to a place in memory.
+const GameStatePtr = *anyopaque;
+
+// TODO: point these the relevant functions inside the game DLL.
+var gameInit: *const fn () GameStatePtr = undefined;
+var gameReload: *const fn (GameStatePtr) void = undefined;
+var gameTick: *const fn (GameStatePtr) void = undefined;
+var gameDraw: *const fn (GameStatePtr) void = undefined;
+
 pub fn main() !void {
-    std.debug.print("Hello world", .{});
-    rl.InitWindow(400, 400, "TEMPLATE");
-    rl.SetTargetFPS(60);
-
-    while (!rl.WindowShouldClose()) {
-        rl.BeginDrawing();
-
-        rl.EndDrawing();
+    loadGameDll() catch @panic("Failed to load the dylib");
+    const game_state = gameInit();
+    c.InitWindow(screen_w, screen_h, "Zig Hot-Reload");
+    c.SetTargetFPS(60);
+    while (!c.WindowShouldClose()) {
+        if (c.IsKeyPressed(c.KEY_R)) {
+            unloadGameDll() catch unreachable;
+            recompileGameDll() catch {
+                std.debug.print("Failed to recompile the dylib", .{});
+            };
+            loadGameDll() catch @panic("Failed to load the dylib");
+            gameReload(game_state);
+        }
+        gameTick(game_state);
+        c.BeginDrawing();
+        gameDraw(game_state);
+        c.EndDrawing();
     }
+    c.CloseWindow();
+}
 
-    rl.CloseWindow();
+var game_dyn_lib: ?std.DynLib = null;
+fn loadGameDll() !void {
+    if (game_dyn_lib != null) return error.AlreadyLoaded;
+
+    const cwd = try std.fs.cwd().realpathAlloc(std.heap.page_allocator, ".");
+    std.log.info("current working directory: {s}", .{cwd});
+
+    var dyn_lib = std.DynLib.open("./zig-out/lib/libgame.dylib") catch {
+        return error.OpenDLLFail;
+    };
+
+    game_dyn_lib = dyn_lib;
+
+    gameInit = dyn_lib.lookup(@TypeOf(gameInit), "gameInit") orelse return error.LookupFail;
+    gameReload = dyn_lib.lookup(@TypeOf(gameReload), "gameReload") orelse return error.LookupFail;
+    gameTick = dyn_lib.lookup(@TypeOf(gameTick), "gameTick") orelse return error.LookupFail;
+    gameDraw = dyn_lib.lookup(@TypeOf(gameDraw), "gameDraw") orelse return error.LookupFail;
+    std.debug.print("Loaded game.dll\n", .{});
+}
+
+fn unloadGameDll() !void {
+    if (game_dyn_lib) |*dyn_lib| {
+        dyn_lib.close();
+        game_dyn_lib = null;
+    } else {
+        return error.AlreadyUnloaded;
+    }
+}
+
+fn recompileGameDll() !void {
+    const process_args = [_][]const u8{
+        "zig",
+        "build",
+        "-Dhot_reload=true",
+    };
+
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+    var build_process = std.process.Child.init(&process_args, allocator);
+    try build_process.spawn();
+
+    const term = try build_process.wait();
+    switch (term) {
+        .Exited => |exited| {
+            if (exited == 2) return error.RecompileFail;
+        },
+        else => return,
+    }
 }
