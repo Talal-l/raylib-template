211cf854171a3e0e433d595c8189ed89e8dc09bd
HamnYce <hamadboa@gmail.com>
2024-10-14 21:14:29 +0300

    Refactor game module: Remove unused code and clean up initialization

--- src/game.zig
+++ src/game.zig
@@ -4,18 +4,14 @@ const utils = @import("utils.zig");
 const game_screen = @import("game_screen/game_screen.zig");
 
 pub const GameState = struct {
-    allocator: std.mem.Allocator,
     // GAME SCREEN STATE
     game_screen_state: game_screen.GameScreenState,
-    // STATE FOR THE NEXT STATE
 };
 
 export fn gameInit() *anyopaque {
-    var allocator = std.heap.page_allocator;
-    const game_state = allocator.create(GameState) catch @panic("Out of memory");
+    const game_state = std.heap.page_allocator.create(GameState) catch @panic("Out of memory");
 
     game_state.* = GameState{
-        .allocator = allocator,
         .game_screen_state = game_screen.GameScreenState.init(),
     };
 
@@ -25,19 +21,20 @@ export fn gameInit() *anyopaque {
 // this is called after the dll is rebuilt
 export fn gameReload(game_state_ptr: *anyopaque) void {
     const game_state: *GameState = @ptrCast(@alignCast(game_state_ptr));
+
     game_state.* = GameState{
-        .allocator = game_state.allocator,
         .game_screen_state = game_screen.GameScreenState.init(),
     };
 }
 
 export fn gameTick(game_state_ptr: *anyopaque) void {
     const game_state: *GameState = @ptrCast(@alignCast(game_state_ptr));
+
     game_state.game_screen_state.update();
 }
 
 export fn gameDraw(game_state_ptr: *anyopaque) void {
     const game_state: *GameState = @ptrCast(@alignCast(game_state_ptr));
+
     game_state.game_screen_state.draw();
-    rl.ClearBackground(rl.BLACK);
 }
