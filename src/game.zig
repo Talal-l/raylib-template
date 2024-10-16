2f12fc6a91e90ce243c4df00172f5de9f1897207
HamnYce <hamadboa@gmail.com>
2024-10-16 19:46:20 +0300

    Refactor game.zig: Add menu screen functionality

--- src/game.zig
+++ src/game.zig
@@ -5,16 +5,19 @@ const utils = @import("utils.zig");
 // importing screens
 const game_screen = @import("game_screen/game_screen.zig");
 const intro_screen = @import("intro_screen/intro_screen.zig");
+const menu_screen = @import("menu_screen/menu_screen.zig");
 
 // screen enum
 const Screen = enum {
     IntroScreen,
+    MenuScreen,
     GameScreen,
 };
 
 // STATES
 pub const GameState = struct {
     intro_screen_state: intro_screen.IntroScreenState,
+    menu_screen_state: menu_screen.MenuScreenState,
     game_screen_state: game_screen.GameScreenState,
     current_screen: Screen,
 };
@@ -23,8 +26,9 @@ export fn gameInit() *anyopaque {
     const game_state = std.heap.page_allocator.create(GameState) catch @panic("Out of memory");
 
     game_state.* = GameState{
-        .game_screen_state = game_screen.GameScreenState.init(),
         .intro_screen_state = intro_screen.IntroScreenState.init(),
+        .menu_screen_state = menu_screen.MenuScreenState.init(),
+        .game_screen_state = game_screen.GameScreenState.init(),
         .current_screen = .IntroScreen,
     };
 
@@ -38,7 +42,7 @@ export fn gameReload(game_state_ptr: *anyopaque) void {
 }
 
 // TODO: implement full game reload
-fn fullGameReload(game_state_ptr: *anyopaque) void {
+fn __fullGameReload(game_state_ptr: *anyopaque) void {
     const game_state: *GameState = @ptrCast(@alignCast(game_state_ptr));
 
     _ = game_state;
@@ -51,6 +55,12 @@ export fn gameTick(game_state_ptr: *anyopaque) void {
         .IntroScreen => {
             game_state.intro_screen_state.update();
             if (game_state.intro_screen_state.done) {
+                game_state.current_screen = .MenuScreen;
+            }
+        },
+        .MenuScreen => {
+            game_state.menu_screen_state.update();
+            if (game_state.menu_screen_state.done) {
                 game_state.current_screen = .GameScreen;
             }
         },
@@ -63,6 +73,7 @@ export fn gameDraw(game_state_ptr: *anyopaque) void {
 
     switch (game_state.current_screen) {
         .IntroScreen => game_state.intro_screen_state.draw(),
+        .MenuScreen => game_state.menu_screen_state.draw(),
         .GameScreen => game_state.game_screen_state.draw(),
     }
 }
--- src/menu_screen/menu_screen.zig
+++ src/menu_screen/menu_screen.zig
@@ -1,22 +1,87 @@
 const std = @import("std");
 const rl = @import("../rl.zig");
+const utils = @import("../utils.zig");
 
-const MenuScreenState = struct {
-    current_option_index: u8,
+var menu_text_size: c_int = undefined;
+const option_texts = [_][*c]const u8{ "START", "EXIT" };
+const menu_screen_title = "Menu Screen";
+const menu_screen_title_font_size = 60;
+
+pub const MenuScreenState = struct {
+    current_option_index: i32,
+    done: bool,
 
     pub fn init() MenuScreenState {
+        menu_text_size = rl.MeasureText(menu_screen_title, menu_screen_title_font_size);
+
         return .{
+            .done = false,
             .current_option_index = 0,
         };
     }
 
     pub fn update(self: *MenuScreenState) void {
-        _ = self;
+        if (rl.IsKeyPressed(rl.KEY_ENTER)) {
+            if (self.current_option_index == 0) {
+                self.done = true;
+            } else if (self.current_option_index == 1) {
+                // TODO: change this so that it exits properly
+                rl.CloseWindow();
+            }
+        }
+
+        if (rl.IsKeyPressed(rl.KEY_UP))
+            self.current_option_index -= 1;
+
+        if (rl.IsKeyPressed(rl.KEY_DOWN))
+            self.current_option_index += 1;
+
+        if (self.current_option_index < 0)
+            self.current_option_index = 0;
+
+        if (self.current_option_index >= option_texts.len)
+            self.current_option_index = option_texts.len - 1;
     }
 
     pub fn draw(self: *MenuScreenState) void {
-        rl.ClearBackground(rl.RAYWHITE);
-        rl.DrawText("Menu Screen", @divTrunc(rl.GetScreenWidth(), 2), @divTrunc(rl.GetScreenHeight(), 2), 70, rl.BLACK);
-        _ = self;
+        rl.ClearBackground(rl.BLACK);
+        rl.DrawText(
+            menu_screen_title,
+            @as(c_int, @intFromFloat(utils.getHalfScreenWidth())) - @divTrunc(menu_text_size, 2),
+            0,
+            menu_screen_title_font_size,
+            rl.WHITE,
+        );
+
+        for (0.., option_texts) |i, option_text| {
+            const option_text_size = rl.MeasureTextEx(
+                rl.GetFontDefault(),
+                option_text,
+                40,
+                1,
+            );
+            const option_text_pos_x: c_int = @intFromFloat(utils.getHalfScreenWidth() - option_text_size.x / 2);
+            const option_text_pos_y: c_int = @intFromFloat(utils.getHalfScreenHeight() + @as(f32, @floatFromInt(i)) * option_text_size.y);
+
+            rl.DrawText(
+                option_text,
+                option_text_pos_x,
+                option_text_pos_y,
+                40,
+                rl.WHITE,
+            );
+
+            if (self.current_option_index == i) {
+                rl.DrawRectangleRec(
+                    .{
+                        .x = @floatFromInt(option_text_pos_x),
+                        .y = @floatFromInt(option_text_pos_y),
+                        .width = option_text_size.x,
+                        .height = option_text_size.y + 5,
+                    },
+                    .{ .r = 255, .g = 255, .b = 255, .a = 100 },
+                );
+            }
+        }
     }
 };
